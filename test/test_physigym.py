1e221c27231ca76ca92598e3800216823b8ea749
bue <epbucher@iu.edu>
2025-01-20 22:00:23 -0500

    @ physigym : in the middle of github workflow pytest unittest implementation.

---
+++ test/test_physigym.py
@@ -0,0 +1,103 @@
+###
+# title: test_cli_2d.py
+#
+# language: python3 and command line
+# author: Alexandre Bertin, Elmar Bucher
+# date: 20235-01-20
+# license: BSD 3-Clause
+#
+# description:
+#   pytest unit test library for the physigym modeling framework.
+#   + https://docs.pytest.org/
+#
+# note:
+#   assert actual == expected, message
+#   == value equality
+#   is reference equality
+#   pytest.approx for real values
+#####
+
+
+# load library
+import os
+import subprocess
+import shutil
+
+# const
+s_path_test = os.getcwd()
+s_path_physigym = '/'.join(s_path_test.replace('\\','/').split('/')[:-1])
+s_path_physicell = '/'.join(s_path_test.replace('\\','/').split('/')[:-2] + ['PhysiCell'])
+
+
+# function
+
+class TestPhysigymTemplate(object):
+    ''' tests for the physigym template model. '''
+
+    def test_physigym_template_classic(self):
+        os.chdir(s_path_physigym)
+        s_result = subprocess.run(['python3', 'install_physigym.py', 'template', '-f'], check=False, capture_output=True)
+        os.chdir(s_path_physicell)
+        shutil.rmtree('output/', ignore_errors=True)
+        s_result = subprocess.run(['make', 'data-cleanup', 'clean', 'reset'], check=False, capture_output=True)
+        s_result = subprocess.run(['make', 'load', 'PROJ=physigym_template'], check=False, capture_output=True)
+        s_result = subprocess.run(['make', 'classic','-j8'], check=False, capture_output=True)
+        s_result = subprocess.run(['./project'], check=False, capture_output=True)
+        #print(s_result)
+        assert(os.path.exists('output/episode00000000/output00000024.xml')) and \
+              (os.path.exists('output/episode00000001/output00000024.xml')) and \
+              (os.path.exists('output/episode00000002/output00000024.xml')) and \
+              (os.path.exists('output/episode00000003/output00000024.xml'))
+        shutil.rmtree('output/')
+
+    def test_physigym_template_embedded(self):
+        assert False
+
+
+class TestPhysigymTutorial(object):
+    ''' tests for the physigym tutorial model. '''
+
+    def test_physigym_tutorial_classic(self):
+        os.chdir(s_path_physigym)
+        s_result = subprocess.run(['python3', 'install_physigym.py', 'tutorial', '-f'], check=False, capture_output=True)
+        os.chdir(s_path_physicell)
+        shutil.rmtree('output/', ignore_errors=True)
+        s_result = subprocess.run(['make', 'data-cleanup', 'clean', 'reset'], check=False, capture_output=True)
+        s_result = subprocess.run(['make', 'load', 'PROJ=physigym_tutorial'], check=False, capture_output=True)
+        s_result = subprocess.run(['make', 'classic','-j8'], check=False, capture_output=True)
+        s_result = subprocess.run(['./project'], check=False, capture_output=True)
+        #print(s_result)
+        assert(os.path.exists('output/episode00000000/output00000024.xml')) and \
+              (os.path.exists('output/episode00000001/output00000024.xml')) and \
+              (os.path.exists('output/episode00000002/output00000024.xml')) and \
+              (os.path.exists('output/episode00000003/output00000024.xml'))
+        shutil.rmtree('output/')
+
+    def test_physigym_tutorial_embedded(self):
+        assert False
+
+
+class TestPhysigymEpisode(object):
+    ''' tests for the physigym episode model. '''
+    def test_physigym_episode_classic_nonrandom(self):
+        os.chdir(s_path_physigym)
+        s_result = subprocess.run(['python3', 'install_physigym.py', 'episode', '-f'], check=False, capture_output=True)
+        os.chdir(s_path_physicell)
+        shutil.rmtree('output/', ignore_errors=True)
+        s_result = subprocess.run(['make', 'data-cleanup', 'clean', 'reset'], check=False, capture_output=True)
+        s_result = subprocess.run(['make', 'load', 'PROJ=physigym_episode'], check=False, capture_output=True)
+        s_result = subprocess.run(['make', 'classic','-j8'], check=False, capture_output=True)
+        s_result = subprocess.run(['./project'], check=False, capture_output=True)
+        #print(s_result)
+        assert(os.path.exists('output/episode00000000/output00000024.xml')) and \
+              (os.path.exists('output/episode00000001/output00000024.xml')) and \
+              (os.path.exists('output/episode00000002/output00000024.xml')) and \
+              (os.path.exists('output/episode00000003/output00000024.xml'))
+        shutil.rmtree('output/')
+
+    def test_physigym_episode_classic_random(self):
+        assert False
+
+    def test_physigym_episode_embedded(self):
+        assert False
+
